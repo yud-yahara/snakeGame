1e21fe13b9197dc85f167d6ef3cba6522cf383e8
yud-yahara <yahara-y@macnica.co.jp>
2023-05-30 13:19:13 +0900

    add game base code created chatGPT

---
+++ snake.js
@@ -0,0 +1,89 @@
+// ゲームの設定
+const canvas = document.getElementById("gameCanvas");
+const context = canvas.getContext("2d");
+const gridSize = 20;
+const gridCount = canvas.width / gridSize;
+let snake = [{ x: 0, y: 0 }];
+let direction = "right";
+let gameLoop;
+
+// キーボードイベントの処理
+document.addEventListener("keydown", changeDirection);
+
+// 初期化
+function init() {
+    snake = [{ x: 0, y: 0 }];
+    direction = "right";
+    clearInterval(gameLoop);
+    gameLoop = setInterval(update, 150);
+}
+
+// ゲームの更新
+function update() {
+    moveSnake();
+    if (checkCollision()) {
+        clearInterval(gameLoop);
+        alert("Game Over!");
+        init();
+    }
+    draw();
+}
+
+// スネークの移動
+function moveSnake() {
+    const head = { x: snake[0].x, y: snake[0].y };
+
+    if (direction === "right") head.x += gridSize;
+    if (direction === "left") head.x -= gridSize;
+    if (direction === "up") head.y -= gridSize;
+    if (direction === "down") head.y += gridSize;
+
+    snake.unshift(head);
+    snake.pop();
+}
+
+// 衝突判定
+function checkCollision() {
+    const head = snake[0];
+    if (
+        head.x < 0 ||
+        head.x >= canvas.width ||
+        head.y < 0 ||
+        head.y >= canvas.height
+    ) {
+        return true;
+    }
+    return false;
+}
+
+// 描画
+function draw() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    snake.forEach(segment => {
+        context.fillStyle = "green";
+        context.fillRect(segment.x, segment.y, gridSize, gridSize);
+    });
+}
+
+// 方向変更
+function changeDirection(event) {
+    const keyPressed = event.keyCode;
+    const validKeys = {
+        37: "left",
+        38: "up",
+        39: "right",
+        40: "down"
+    };
+
+    if (validKeys[keyPressed]) {
+        if (direction === "up" && validKeys[keyPressed] === "down") return;
+        if (direction === "down" && validKeys[keyPressed] === "up") return;
+        if (direction === "left" && validKeys[keyPressed] === "right") return;
+        if (direction === "right" && validKeys[keyPressed] === "left") return;
+
+        direction = validKeys[keyPressed];
+    }
+}
+
+// ゲームの開始
+init();
